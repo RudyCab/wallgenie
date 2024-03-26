0c8905ae6b38b9188711d46069f9c8f014cb852a
gabe-casanova <gabecasanova7@gmail.com>
2024-03-26 16:21:40 -0500

    added dependency to package

--- src/WallEditor/WallEditorPage.jsx
+++ src/WallEditor/WallEditorPage.jsx
@@ -1,50 +1,93 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import SettingsPopup from "../Components/SettingsPopup/SettingsPopup";
-import SplitPane from 'react-split-pane';
+import SplitPane from "react-split-pane";
 import "./WallEditorPage.css";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function WallEditorPage() {
-  const [topPaneSize, setTopPaneSize] = useState('65%');
+  const [topPaneSize, setTopPaneSize] = useState("65%");
 
-  const handleResize = size => {
+  const handleResize = (size) => {
     setTopPaneSize(size);
   };
 
   return (
     <div>
-
-      <div className='split-pane-wrapper' style={{ pointerEvents: 'auto' }}>
+      <div className="split-pane-wrapper" style={{ pointerEvents: "auto" }}>
         <h1>Wall Editor Page</h1>
         <SettingsPopup />
 
-        <div className='split-pane-wrapper'>
-          <SplitPane split="horizontal" size={topPaneSize} onChange={size => handleResize(size)}>
-            <div style={{background:"#F9F9F9", height:'100%'}}></div>
-            <div style={{background:"#215F5F", height:'100%', overflowY: 'auto'}}>
-              <div class="row" style={{ paddingTop: '10px', paddingLeft: '10px', paddingRight: '10px'}}>
-                  <div class="col-8">
-                      <div className='left' style={{fontSize: '20px', color: 'white', fontWeight: 'bold'}}>
-                        <p>Wall Decor</p>
-                      </div>
+        <div className="split-pane-wrapper">
+          <SplitPane
+            split="horizontal"
+            size={topPaneSize}
+            onChange={(size) => handleResize(size)}
+          >
+            <div style={{ background: "#F9F9F9", height: "100%" }}></div>
+            <div
+              style={{
+                background: "#215F5F",
+                height: "100%",
+                overflowY: "auto",
+              }}
+            >
+              <div
+                class="row"
+                style={{
+                  paddingTop: "10px",
+                  paddingLeft: "10px",
+                  paddingRight: "10px",
+                }}
+              >
+                <div class="col-8">
+                  <div
+                    className="left"
+                    style={{
+                      fontSize: "20px",
+                      color: "white",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    <p>Wall Decor</p>
                   </div>
-                  <div class="col-4">
-                      <button className="rounded" style={{float:'right'}}>Select</button>
-                  </div>
+                </div>
+                <div class="col-4">
+                  <button className="rounded" style={{ float: "right" }}>
+                    Select
+                  </button>
+                </div>
               </div>
               <div className="image-container">
-                <img src={require("../WallEditorImages/Item1.png")} alt="Item 1" className="image"/>
-                <img src={require("../WallEditorImages/Item2.jpeg")} alt="Item 2" className="image"/>
-                <img src={require("../WallEditorImages/Item3.png")} alt="Item 3" className="image"/>
-                <img src={require("../WallEditorImages/Item4.png")} alt="Item 4" className="image"/>
-                <img src={require("../WallEditorImages/Item5.jpg")} alt="Item 5" className="image"/>
+                <img
+                  src={require("../WallEditorImages/Item1.png")}
+                  alt="Item 1"
+                  className="image"
+                />
+                <img
+                  src={require("../WallEditorImages/Item2.jpeg")}
+                  alt="Item 2"
+                  className="image"
+                />
+                <img
+                  src={require("../WallEditorImages/Item3.png")}
+                  alt="Item 3"
+                  className="image"
+                />
+                <img
+                  src={require("../WallEditorImages/Item4.png")}
+                  alt="Item 4"
+                  className="image"
+                />
+                <img
+                  src={require("../WallEditorImages/Item5.jpg")}
+                  alt="Item 5"
+                  className="image"
+                />
               </div>
             </div>
           </SplitPane>
         </div>
       </div>
-
-      
     </div>
   );
 }
