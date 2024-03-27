3883b8669508e383673d82ae6efbeaae99ba9e74
RudyCab <caballero.rudy@utexas.edu>
2024-03-26 19:26:36 -0500

    new project buttons

--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -1,10 +1,19 @@
 // HomePage.js
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./HomePage.css";
 
 function HomePage() {
+  const newProjctClick = () => {
+    // Go to wall Editor page
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
+      <Link to="/WallEditor">
+        <button className="NewProjectButt">New Project</button>
+      </Link>
     </div>
   );
 }
