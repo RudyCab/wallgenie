f90cdc2bbcba1f5c17c978112bff35f4c617de9d
gabe-casanova <gabecasanova7@gmail.com>
2024-04-07 17:57:04 -0500

    fixed walleditor styling, added new __DISPLAY_SLIFER debug bool

--- src/WallEditor/WallEditorPage.jsx
+++ src/WallEditor/WallEditorPage.jsx
@@ -1,21 +1,19 @@
 import React, { useState, useRef } from "react";
 import SettingsPopup from "../Components/SettingsPopup/SettingsPopup";
-import SplitPane from "react-split-pane";
-import Draggable from './Draggable'
+import WallComponent from "../Components/Wall/WallComponent";
+import Draggable from "./Draggable";
 import "./WallEditorPage.css";
 import "bootstrap/dist/css/bootstrap.css";
-import WallComponent from "../Components/Wall/WallComponent";
 import { Wall } from "../Structs/Wall";
 import { IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 function WallEditorPage() {
   const [alertDisplayed, setAlertDisplayed] = useState(false);
-  const [topPaneSize, setTopPaneSize] = useState("65%");
   const parentRef = useRef(null);
 
-  const handleResize = (size) => {
-    setTopPaneSize(size);
-  };
+  // for debugging purposess
+  const __DISPLAY_SLIDER = false;
 
   // Wall Constants
   let PADDING = 20; // left, right padding
@@ -31,8 +29,9 @@ function WallEditorPage() {
   }
 
   // Create new `Wall` instance
+  const VERTICAL_OFFSET = -60;
   const wall = new Wall(
-    { x: x, y: window.innerHeight * 0.25 - 120},
+    { x: x, y: window.innerHeight * 0.25 + VERTICAL_OFFSET },
     {
       width: wall_width,
       height: (3 / 4) * wall_width,
@@ -47,6 +46,12 @@ function WallEditorPage() {
     [],
     { PADDING: PADDING, MAX_WIDTH: MAX_WIDTH } // constants
   );
+
+  // back button navigation
+  const navigate = useNavigate();
+  const handleBackButtonClick = () => {
+    navigate(-1);
+  };
 
   /* this container should contain the following components:
     x - back button
@@ -63,73 +68,72 @@ function WallEditorPage() {
         style={{ pointerEvents: "auto" }}
       >
         <div ref={parentRef} className="top-row" id="toprow">
-          <button className="back-button" disabled={alertDisplayed}>
+          <button
+            className="back-button"
+            disabled={alertDisplayed}
+            onClick={handleBackButtonClick}
+          >
             <IoIosArrowBack />
           </button>
-        </div>
-        <div style={{height: "450px"}}>
-        <WallComponent wall={wall} />
-        </div>
-        <SettingsPopup
+          <SettingsPopup
             alertDisplayed={alertDisplayed}
             setAlertDisplayed={setAlertDisplayed}
           />
-        {/* SPLIT PAGE STUFF */}
-        {/* <div className="split-pane-wrapper">
-          <SplitPane
-            split="horizontal"
-            size={topPaneSize}
-            onChange={(size) => handleResize(size)}
-          > */}
-            {/* <div style={{ background: "#F9F9F9", height: "100%" }}></div> */}
-            <div
-              style={{
-                background: "#215F5F",
-                height: "100%",
-                overflowY: "auto",
-              }}
-            >
-              <div
-                class="row"
-                style={{
-                  paddingTop: "10px",
-                  paddingLeft: "10px",
-                  paddingRight: "10px",
-                }}
-              >
-                 {/* <div>
-                   <div
-                     className="left"
-                     style={{
-                       fontSize: "20px",
-                       color: "white",
-                       fontWeight: "bold",
-                    }}
-                  >
-                    <p>Wall Decor</p>
-                  </div>
-                </div> */}
-              </div>
-              <div className="image-container">
-                
-                <Draggable img="./WallEditorImages/Item1.png" alt="Item 1"
-                  className="image" x={0}></Draggable>
-
-                <Draggable img="./WallEditorImages/Item2.jpeg" alt="Item 2"
-                  className="image" x={150}></Draggable>
-
-                <Draggable img="./WallEditorImages/Item3.png" alt="Item 3"
-                  className="image" x={250}></Draggable>
-
-                <Draggable img="./WallEditorImages/Item4.png" alt="Item 4"
-                  className="image" x={350}></Draggable>
-               
-               <Draggable img="./WallEditorImages/Item5.jpg" alt="Item 5"
-                  className="image" x={450}></Draggable>
-              </div>
-            {/* </div>
-          </SplitPane> */}
         </div>
+        <div style={{ height: "450px" }}>
+          <WallComponent wall={wall} />
+        </div>
+        
+        {__DISPLAY_SLIDER && (
+          <div
+            style={{
+              background: "#215F5F",
+              height: "100%",
+              overflowY: "auto",
+            }}
+          >
+            <div
+              class="row"
+              style={{
+                paddingTop: "10px",
+                paddingLeft: "10px",
+                paddingRight: "10px",
+              }}
+            />
+            <div className="image-container">
+              <Draggable
+                img="./WallEditorImages/Item1.png"
+                alt="Item 1"
+                className="image"
+                x={0}
+              />
+              <Draggable
+                img="./WallEditorImages/Item2.jpeg"
+                alt="Item 2"
+                className="image"
+                x={150}
+              />
+              <Draggable
+                img="./WallEditorImages/Item3.png"
+                alt="Item 3"
+                className="image"
+                x={250}
+              />
+              <Draggable
+                img="./WallEditorImages/Item4.png"
+                alt="Item 4"
+                className="image"
+                x={350}
+              />
+              <Draggable
+                img="./WallEditorImages/Item5.jpg"
+                alt="Item 5"
+                className="image"
+                x={450}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
