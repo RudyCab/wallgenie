1f5ddb8328d3eec11930bb647d9f1578fee2ddde
RudyCab <caballero.rudy@utexas.edu>
2024-04-07 21:41:06 -0500

    centered home items

--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -1,8 +1,8 @@
-// HomePage.js
 import { Link } from "react-router-dom";
 import "./HomePage.css";
 import React, { useEffect } from "react";
-import logo from "../logo.png"; // Import the image file
+import logo from "../logo.png"; 
+import CurrentProjectCard from "../Components/CurrentProjectCard/CurrentProjectCard" 
 
 function HomePage() {
   useEffect(() => {
@@ -16,11 +16,9 @@ function HomePage() {
     <div>
       <img src={logo} alt="logo of name and genie lamp" />
       <div className="homepage-button-container">
-        <button className="homepage-buttons" style={{ marginRight: 10 }}>
-          Current Project
-        </button>
-        <div></div>
-        <Link to="/WallEditor">
+      <CurrentProjectCard 
+          imagePath="path_to_your_current_project_image"
+        />        <Link to="/WallEditor">
           <button className="homepage-buttons">New Project</button>
         </Link>
       </div>
